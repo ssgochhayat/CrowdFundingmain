323d29588ce509fda30e252afc1e7da370e3027d
Suman Kumar Gochhayat <ssgochhayat2018@gmail.com>
2025-02-23 19:51:12 +0530

    first commit

---
+++ Client/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h2>React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library[5][6] that aims to make building user interfaces based on components more "seamless".[5] It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.</h2>
+    </div>
+  )
+}
+
+export default Contact
