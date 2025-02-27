d3c60f0d6e91860a77f366396a2d4a57417bd058
Suman Kumar Gochhayat <ssgochhayat2018@gmail.com>
2025-02-27 18:06:40 +0530

    3rd commit

---
+++ crowdfunding-app/backend/routes/auth.js
@@ -0,0 +1,1 @@
+const authRoutes = require('./routes/auth');
--- crowdfunding-app/backend/server.js
+++ crowdfunding-app/backend/server.js
@@ -13,7 +13,10 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
